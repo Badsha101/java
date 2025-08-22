28a37cd9c3ebdb993ca6c7dd0a3f4ac183430d28
Badsha101 <badshahalamgir101@gmail.com>
2025-08-22 15:56:09 +0200

    JDBC successfull

---
+++ basic/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass{
+
+public static void main(String[] args){
+
+
+    System.out.println("Hello.. form my class");
+
+   int sum = add(9, 3);
+   System.out.println("sum = "+sum);
+}
+static int add(int a, int b){
+    return a+b;
+}
+}

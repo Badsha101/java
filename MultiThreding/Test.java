87ab4381b7633c6e94c72c43f98bc74504520e78
Badsha101 <badshahalamgir101@gmail.com>
2025-08-24 21:21:44 +0200

    MultiThreading updated

--- MultiThreding/Test.java
+++ MultiThreding/Test.java
@@ -1,22 +1,36 @@
-class MyCharacter extends Thread{
+class MyNumber implements Runnable{
 
   public void run(){
 
   for(int i = 1; i <= 10; i++){
 
     System.out.print(i+", ");
+     // Thread.yield();
+     try{
+          Thread.sleep(1000);
+     }catch(Exception e){
+      System.out.println(e);
 
+     }
     }
 
     }
 
   }
     
-class MyNumber extends Thread{
+class MyCharacter extends Thread{
   public void run(){
     for(char i = 'A'; i <= 'J'; i++){
     
     System.out.print(i+ ", ");
+    Thread.yield();
+
+     try{
+            Thread.sleep(1000);
+       }catch(Exception e){
+        System.out.println(e);
+ 
+       }
 
       }
 
@@ -31,13 +45,13 @@ public class Test{
 public static void main(String[] args){
 
  MyNumber myNumber = new MyNumber();
-// Thread myThread = new Thread(myNumber);
+ Thread myThread = new Thread(myNumber);
 
  MyCharacter myCharacter = new MyCharacter();
 
 
 
- myNumber.start();
+ myThread.start();
  myCharacter.start();
 
 
